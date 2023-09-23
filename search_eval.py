65eeca73fbc9b6352b025569fa37824e0500ea95
Arshad Jahir Hussain <arshadjahirhussain@arshads-mbp.myfiosgateway.com>
2023-09-23 13:09:27 -0400

    fix

--- search_eval.py
+++ search_eval.py
@@ -1,10 +1,11 @@
 import math
 import sys
 import time
-# from scipy import stats
 
 import metapy
 import pytoml
+
+import numpy as np
 
 class InL2Ranker(metapy.index.RankingFunction):
     """
@@ -21,11 +22,8 @@ class InL2Ranker(metapy.index.RankingFunction):
         For fields available in the score_data sd object,
         @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
         """
-        # return (self.param + sd.doc_term_count) / (self.param * sd.doc_unique_terms + sd.doc_size)
-    
-        tfn = sd.doc_term_count * math.log((1 + (sd.avg_dl / sd.doc_size)), 2)
-        return sd.query_term_weight * (tfn / (tfn + self.param)) * math.log(((sd.num_docs + 1) / (sd.corpus_term_count + 1 / 2)), 2)
-
+        tfn = sd.doc_term_count * math.log((1+(sd.avg_dl/sd.doc_size)),2)
+        return sd.query_term_weight * (tfn / (tfn + self.param)) * math.log(((sd.num_docs + 1) / (sd.corpus_term_count + 0.5)),2)
 
 def load_ranker(cfg_file):
     """
@@ -33,7 +31,7 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index. You can ignore this for MP2.
     """
-    return metapy.index.JelinekMercer()
+    return InL2Ranker()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -60,6 +58,7 @@ if __name__ == '__main__':
     query_start = query_cfg.get('query-id-start', 0)
 
     result_inl2, result_bm25 = [],[]
+
     query = metapy.index.Document()
     print('Running queries')
     with open(query_path) as query_file:
@@ -67,19 +66,21 @@ if __name__ == '__main__':
             query.content(line.strip())
             results = ranker.score(idx, query, top_k)
             avg_p = ev.avg_p(results, query_start + query_num, top_k)
-            result_inl2.append(format(avg_p))
+            result_inl2.append(format(avg_p))#append the avg to a list
             print("Query {} average precision: {}".format(query_num + 1, avg_p))
     print("Mean average precision: {}".format(ev.map()))
     print("Elapsed: {} seconds".format(round(time.time() - start_time, 4)))
 
     query = metapy.index.Document()
-    ranker_bm25 = metapy.index.OkapiBM25(k1 = 6 / 5, b = 3 / 4, k3 = 500)
+    ranker_bm25 = metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=500)
+    
     ev_bm25 = metapy.index.IREval('config.toml')
+    
     with open('config.toml', 'r') as fin:
             cfg_d = pytoml.load(fin)
-    
     query_cfg = cfg_d['query-runner']
     query_start = query_cfg.get('query-id-start', 0)
+    
     num_results = 10
     with open('cranfield-queries.txt') as query_file:
         for query_num, line in enumerate(query_file):
@@ -88,16 +89,14 @@ if __name__ == '__main__':
             avg_p_bm25 = ev_bm25.avg_p(results, query_start + query_num, num_results)
             result_bm25.append(format(avg_p_bm25))
     ev_bm25.map()
-    
+
     with open ('inl2.avg_p.txt','w') as output:
         for item in result_inl2:
-            output.write('%s\n' % item)
+            output.write("%s\n" % item)
 
     with open ('bm25.avg_p.txt','w') as output:
         for item in result_bm25:
-            output.write('%s\n' % item)
+            output.write("%s\n" % item)
 
-    result_inl2 = list(map(float,result_inl2))
-    result_bm25 = list(map(float,result_bm25))
-    # with open ('significance.txt','w') as output:
-    #     output.write("%s" % stats.ttest_rel(result_inl2, result_bm25)[1])
+    result_inl2 = np.array(map(float,result_inl2))
+    result_bm25 = np.array(map(float,result_bm25))
